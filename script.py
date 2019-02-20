c7308b7ebf21b12ee59e7b6dce6a28132235cac5
idcpp1995 <ppcdi1995@gmail.com>
2019-02-20 14:14:30 +0200

    screen 3.png

--- script.py
+++ script.py
@@ -1,38 +1,65 @@
 import shutil
 import os
 import argparse
+import re
 
+def new_folder(src_link, dst_link):
+    common_list = os.listdir(path=src_link)  # выгружаем все что есть в папке-источнике
+    dst_folder = os.listdir(path=dst_link)  # выгружаем все что есть в папке-приемнике
+    song_list = []  # все mp3 файлы папки-источника
 
-def mp3_list(src_link):
-    common_list = os.listdir(path=src_link)  # выгружаем все что есть в папке
-    song_list = []
+    copy_counter = 0  # счетчик копий
+    final_list_counter = 0  # счетчик уникальных mp3
+
     for i in common_list:
-        if i.endswith('.mp3'):  # сортируем по мп3 расширению
+        if re.search(r'копія', i):  # проверка на копии
+            path = os.path.join(os.path.abspath(os.path.dirname(src_link)), i)
+            os.remove(path)  # delete копий
+            copy_counter += 1
+        elif i.endswith('.mp3'):  # сортируем по мп3 расширению
             song_list.append(i)
-    return song_list
+    for i in common_list:  # удаляет дубликаты из папки-источника, если такие уже есть в папке-приемнике
+        for j in dst_folder:
+            if i == j:
+                path2 = os.path.join(os.path.abspath(os.path.dirname(src_link)), j)
+                os.remove(path2)
+    print("Копий удалено в размере: " + str(copy_counter))
 
+    a = os.listdir(path=src_link)
+    final_list = []  # список mp3 файлов папки-источника без копий и дубликатов в папке-приемнике
+    for i in a:
+        if i.endswith('.mp3'):
+            final_list.append(i)
+            final_list_counter += 1
+    print("Файлов скопировано в размере: " + str(final_list_counter))
 
-def new_folder(src_link2, src_link, dst_link):
-    song_list = mp3_list(src_link)
-    for i in song_list:
-        shutil.move(src_link2 + i, dst_link)  # папка источник + *.mp3, папка назначения
-
+    if os.path.exists(dst_link):  # проверка на наявность папки-приемника
+        for i in final_list:
+            shutil.move(src_link + i, dst_link)  # папка источник + *.mp3, папка назначения
+    else:  # создание папки-приемника
+        print("Вы указали несуществующее место назначения \n")
+        yes_no = input("Желаете создать папку ?(y/n): ")
+        if yes_no == 'y':
+            newdir = input("Введите адрес для новой папки: ")
+            os.makedirs(newdir)
+            for i in final_list:
+                shutil.move(src_link + i, newdir)
+        else:
+            print("End")
 
 parser = argparse.ArgumentParser()
 
-parser.add_argument('src_link', type=str)
-
-parser.add_argument('src_link2', type=str)
 parser.add_argument('src_link', type=str)
 parser.add_argument('dst_link', type=str)
 args = parser.parse_args()
 
 if __name__ == '__main__':
-    mp3_list(args.src_link)
-    new_folder(args.src_link2, args.src_link, args.dst_link)
+    try:
+        new_folder(args.src_link, args.dst_link)
+    except FileNotFoundError:
+        print("Вы указали неверный источник \nEnd")
 
 """
-mp3_list("C:/Users/Dima/Downloads/py.projects/gitMP3/MP3/music")
-new_folder("C:/Users/Dima/Downloads/py.projects/gitMP3/MP3/music/", "C:/Users/Dima/Downloads/py.projects/gitMP3/MP3/music/", "C:/Users/Dima/Downloads/py.projects/gitMP3/MP3/copy")
-# python script.py C:/Users/Dima/Downloads/py.projects/mp3/music C:/Users/Dima/Downloads/py.projects/mp3/music/ C:/Users/Dima/Downloads/py.projects/mp3/music/ C:/Users/Dima/Downloads/py.projects/mp3/aaaaaaaaaa
+пример парса 
+python script.py C:/Users/Dima/Downloads/py.projects/gitMP3/MP3/music/ C:/Users/Dima/Downloads/py.projects/gitMP3/MP3/copy/
 """
